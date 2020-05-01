7bb1956ad778b635840dfdd9e4204c9951830fb7
Changchang12345 <1612387487@qq.com>
2020-05-01 10:28:44 +0800

    Initial Commit

---
+++ miniprogram/app.js
@@ -0,0 +1,61 @@
+//app.js
+var app = getApp(); 
+App({
+      
+  globalData:{
+   // appid:'wx68262e135382e8cf',//appid需自己提供，此处的appid我随机编写
+    //secret:'acdd38bc20750418c9f7f9527ff9ea61',//secret需自己提供，此处的secret我随机编写
+    op:''
+    
+},
+  onLaunch: function () { var that=this
+    if (!wx.cloud) {
+      console.error('请使用 2.2.3 或以上的基础库以使用云能力')
+    } else {//console.log('chusa'),
+      wx.cloud.init({
+        
+        // env 参数说明：
+        //   env 参数决定接下来小程序发起的云开发调用（wx.cloud.xxx）会默认请求到哪个云环境的资源
+        //   此处请填入环境 ID, 环境 ID 可打开云控制台查看
+        //   如不填则使用默认环境（第一个创建的环境）
+         env: 'dev-115990',
+        traceUser: true,
+      })
+    }
+    wx.login({ 
+      success: res => {
+       // 发送 res.code 到后台换取 openId, sessionKey, unionId
+       if (res.code) {
+        wx.request({
+         url: 'http://localhost:8081/user/login',
+         method: 'POST',
+         data: {
+          code: res.code
+         },
+         header: {
+          'content-type': 'application/x-www-form-urlencoded'
+         },
+         success(res) {
+          console.log("openid:"+res.data.openid);
+          if (res.data.openid != "" || res.data.openid!=null){
+           // 登录成功
+           that.globalData.op=res.data.openid;
+           console.log(that.globalData.op)
+           wx.setStorageSync("openid", res.data.openid);//将用户id保存到缓存中
+           wx.setStorageSync("session_key", res.data.session_key);//将session_key保存到缓存中
+          }else{
+           // 登录失败
+           // TODO 跳转到错误页面，要求用户重试
+    
+    
+           return false;
+          }
+         }
+        })
+       } else {
+        console.log('获取用户登录态失败！' + res.errMsg)
+       }
+      }
+     })
+  }
+})
---
+++ miniprogram/pages/matches/matches.js
@@ -0,0 +1,109 @@
+// pages/chooseLib/chooseLib.js
+//var app = getApp();
+//console.log(app.globalData.op)
+const db = wx.cloud.database({});
+const cont = db.collection('users');
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+      items:[],
+      ne:[],
+      id:[],
+      openid:'',
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+    //console.log(this.data.nickName, this.data.avaUrl)
+  
+  onLoad: function (options) {
+    //console.log(this.data.Myopenid)
+    var that = this;
+    wx.getStorage({//获取本地缓存
+      key:"openid",
+      success:function(res){
+        that.setData({
+          openid:res.data
+        });console.log(that.data.openid)
+      },
+    })
+    //1、引用数据库   
+    const db = wx.cloud.database({
+      //这个是环境ID不是环境名称     
+      env: 'dev-115990'
+    })
+
+    //2、开始查询数据了  news对应的是集合的名称   
+    db.collection('users').get({
+      //如果查询成功的话    
+      success: res => {
+        //这一步很重要，给ne赋值，没有这一步的话，前台就不会显示值      
+        this.setData({
+          ne: res.data,
+          nickName:'',
+          avatarUrl:''
+        })
+      }
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+
+  go(e){
+    console.log(e.currentTarget.dataset);
+    wx.navigateTo({
+      url: '/pages/room/room?id='+e.currentTarget.dataset.id,
+    })
+    //console.log(e);
+  },
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
